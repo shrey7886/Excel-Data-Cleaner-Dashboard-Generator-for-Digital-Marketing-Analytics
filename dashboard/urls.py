73848d9422146b6927d329901bed5d8f98022ef3
Shrey Golekar <shreygolekar@gmail.com>
2025-07-15 07:41:20 -0500

    Restore dashboard/urls.py: add landing page and client portal routes for site functionality

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,7 @@
-urlpatterns = []
-# from .views import rag_chatbot
-# urlpatterns += [
-#     path("api/rag-chatbot/", rag_chatbot, name="rag_chatbot"),
-# ] 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.landing_page, name='landing_page'),
+    path('client-portal/', views.client_portal, name='client_portal'),
+] 
